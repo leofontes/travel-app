5766f5747a7e5f035006b00ee4fa6fadaf0b7de5
Leo Fontes <leofontes@gmail.com>
2016-11-05 15:17:28 -0200

    Salvar favoritos em SharedPreferences

--- app/src/main/java/me/leofontes/dsotravel/Fragments/DetailActivityFragment.java
+++ app/src/main/java/me/leofontes/dsotravel/Fragments/DetailActivityFragment.java
@@ -3,8 +3,10 @@ package me.leofontes.dsotravel.Fragments;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +14,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import me.leofontes.dsotravel.MainActivity;
 import me.leofontes.dsotravel.Models.Attraction;
 import me.leofontes.dsotravel.R;
 
@@ -25,6 +29,8 @@ public class DetailActivityFragment extends Fragment {
     protected Attraction attraction;
     protected SharedPreferences myPrefs;
     protected SharedPreferences.Editor peditor;
+    protected Boolean favorite;
+    protected Button mButtonFavorite;
 
     public DetailActivityFragment() {
     }
@@ -44,6 +50,11 @@ public class DetailActivityFragment extends Fragment {
 
             }
         }
+
+        myPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        peditor = myPrefs.edit();
+
+        favorite = checkFavorites(attraction.getId());
 
         // Set name
         TextView textViewName = (TextView) rootview.findViewById(R.id.textview_detail_name);
@@ -100,8 +111,97 @@ public class DetailActivityFragment extends Fragment {
             }
         });
 
+        // Set up favorite button
+        mButtonFavorite = (Button) rootview.findViewById(R.id.button_detail_favorite);
+        configButton(favorite);
+
+        mButtonFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(favorite) {
+                    peditor.putString(saveFavorite(attraction.getId()), "false");
+                    peditor.commit();
+                    favorite = !favorite;
+                    configButton(favorite);
+
+                    Toast.makeText(getActivity(), attraction.getName() + " removido dos favoritos.", Toast.LENGTH_SHORT).show();
+                } else {
+                    peditor.putString(saveFavorite(attraction.getId()), "true");
+                    peditor.commit();
+                    favorite = !favorite;
+                    configButton(favorite);
+
+                    Toast.makeText(getActivity(), attraction.getName() + " adicionado aos favoritos.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         return rootview;
     }
 
-    
+    private void configButton(Boolean bol) {
+        if(bol) {
+            mButtonFavorite.setText(getString(R.string.detail_button_unfavorite));
+            mButtonFavorite.setBackgroundColor(getResources().getColor(R.color.defaultRed));
+        } else {
+            mButtonFavorite.setText(getString(R.string.detail_button_favorite));
+            mButtonFavorite.setBackgroundColor(getResources().getColor(R.color.defaultLightBlue));
+        }
+    }
+
+    private String saveFavorite(int id) {
+        switch (id) {
+            case 0:
+                return getString(R.string.fav_koxixos);
+            case 1:
+                return getString(R.string.fav_ostradamus);
+            case 2:
+                return getString(R.string.fav_may);
+            case 3:
+                return getString(R.string.fav_guacamole);
+            case 4:
+                return getString(R.string.fav_me);
+            case 5:
+                return getString(R.string.fav_sanduicheria);
+            case 6:
+                return getString(R.string.fav_wamaki);
+            case 7:
+                return getString(R.string.fav_didge);
+            case 8:
+                return getString(R.string.fav_bits);
+            case 9:
+                return getString(R.string.fav_toro);
+            default:
+                return "false";
+        }
+    }
+
+    public boolean checkFavorites(int i) {
+        switch(i) {
+            case 0:
+                return myPrefs.getString(getString(R.string.fav_koxixos), "false").equals("true");
+            case 1:
+                return myPrefs.getString(getString(R.string.fav_ostradamus), "false").equals("true");
+            case 2:
+                return myPrefs.getString(getString(R.string.fav_may), "false").equals("true");
+            case 3:
+                return myPrefs.getString(getString(R.string.fav_guacamole), "false").equals("true");
+            case 4:
+                return myPrefs.getString(getString(R.string.fav_me), "false").equals("true");
+            case 5:
+                return myPrefs.getString(getString(R.string.fav_sanduicheria), "false").equals("true");
+            case 6:
+                return myPrefs.getString(getString(R.string.fav_wamaki), "false").equals("true");
+            case 7:
+                return myPrefs.getString(getString(R.string.fav_didge), "false").equals("true");
+            case 8:
+                return myPrefs.getString(getString(R.string.fav_bits), "false").equals("true");
+            case 9:
+                return myPrefs.getString(getString(R.string.fav_toro), "false").equals("true");
+            default:
+                return true;
+        }
+    }
+
+
 }
